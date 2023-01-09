5947cb79f90e7e4245014f9ffcaeed1b99c9db8a
Siavash <siamhb7@protonmail.com>
2023-01-09 13:31:35 +0330

    implement web socket server in server by ws

--- server/server.js
+++ server/server.js
@@ -4,10 +4,27 @@ const cors = require("cors");
 const { readFileSync, writeFileSync } = require("fs");
 const app = express();
 const dotenv = require("dotenv");
-// const getData = require("./scrapData");
+const getData = require("./scrapData");
+const { Server } = require("ws");
+const getCurrentData = require("./scrapData");
 dotenv.config();
 
-// getCurrentData();
+const wsServer = new Server({ port: 3232 }, () => {
+  console.log("websocket listen on 3232");
+});
+
+wsServer.on("connection", (ws) => {
+  console.log("one socket connected by ", ws);
+});
+
+setInterval(async () => {
+  // get fresh data and send it across all ws
+
+  const data = await getCurrentData();
+  wsServer.clients.forEach((c) => {
+    c.send(JSON.stringify(data));
+  });
+}, 5000);
 
 app.use(
   cors({
@@ -26,5 +43,5 @@ app.get("/", (req, res) => {
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(` server is listening on localhost:${port} `);
+  console.log(`server is listening on localhost:${port} `);
 });
